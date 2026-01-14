b3d5c97463eb46036da03341727dd72be6daa789
Maria070210 <mcatala2002@gmail.com>
2026-01-14 11:11:31 +0100

    02-template-string

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,4 @@
+const firstName = 'Maria';
+const lastName = "O'Neal"; /* Dobles comillas para cuando lo que escribes tiene apostrofe */
+const fullName = `${firstName} ${lastName}`; /* Para concatenar texto */
+console.log(fullName);
